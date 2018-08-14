8940c0b799b15b7d11cb741340018feb063c973e
anorman68 <anorman68@gmail.com>
2018-08-14 09:35:59 -0400

    add mapDispatchToProps with manual wire-up

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -49,8 +49,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -59,6 +59,12 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
 //const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
 //export default connectedStateAndProps(CoursesPage);
-export default connect(mapStateToProps)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
