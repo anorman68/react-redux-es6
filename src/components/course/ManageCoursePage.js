1807ee4caa57996410cbd9368729c09a4f6d63ef
anorman68 <anorman68@gmail.com>
2018-08-15 13:18:01 -0400

    end of module 12

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
 	constructor(props, context) {
@@ -10,41 +11,54 @@ class ManageCoursePage extends React.Component {
 
 		this.state = {
 			course: Object.assign({}, this.props.course),
-			errors: {}
+			errors: {},
+			saving: false
 		};
 
-    this.updateCourseState = this.updateCourseState.bind(this);
-    this.saveCourse = this.saveCourse.bind(this);
+		this.updateCourseState = this.updateCourseState.bind(this);
+		this.saveCourse = this.saveCourse.bind(this);
 	}
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.course.id != nextProps.course.id) {
-      // Necessary to populate from when existing course is loaded directly.
-      this.setState({ course: Object.assign({}, nextProps.course) });
-    }
-  }
+	componentWillReceiveProps(nextProps) {
+		if (this.props.course.id != nextProps.course.id) {
+			// Necessary to populate from when existing course is loaded directly.
+			this.setState({ course: Object.assign({}, nextProps.course) });
+		}
+	}
 
 	updateCourseState(event) {
 		const field = event.target.name;
 		let course = Object.assign({}, this.state.course);
 		course[field] = event.target.value;
 		return this.setState({ course: course });
-  }
+	}
 
-  saveCourse(event) {
-    event.preventDefault();
-    this.props.actions.saveCourse(this.state.course);
-    this.context.router.push('/courses');
-  }
+	saveCourse(event) {
+		event.preventDefault();
+		this.setState({ saving: true });
+    this.props.actions.saveCourse(this.state.course)
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({ saving: false });
+      });
+	}
+
+	redirect() {
+    this.setState({ saving: false });
+    toastr.success('Course saved');
+		this.context.router.push('/courses');
+	}
 
 	render() {
 		return (
 			<CourseForm
 				allAuthors={this.props.authors}
-        onChange={this.updateCourseState}
-        onSave={this.saveCourse}
+				onChange={this.updateCourseState}
+				onSave={this.saveCourse}
 				course={this.state.course}
-				errors={this.state.errors}
+        errors={this.state.errors}
+        saving={this.state.saving}
 			/>
 		);
 	}
@@ -52,29 +66,29 @@ class ManageCoursePage extends React.Component {
 
 ManageCoursePage.propTypes = {
 	course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+	authors: PropTypes.array.isRequired,
+	actions: PropTypes.object.isRequired
 };
 
 //Pull in the React Router context so router is available on this.context.router.
 ManageCoursePage.contextTypes = {
-  router: PropTypes.object
+	router: PropTypes.object
 };
 
 function getCourseById(courses, id) {
-  const course = courses.filter(course => course.id == id);
-  if (course.length) return course[0]; //since filter returns an array, have to grab the first.
-  return null;
+	const course = courses.filter((course) => course.id == id);
+	if (course.length) return course[0]; //since filter returns an array, have to grab the first.
+	return null;
 }
 
 function mapStateToProps(state, ownProps) {
-  const courseId = ownProps.params.id; // from the path `/course/:id`
+	const courseId = ownProps.params.id; // from the path `/course/:id`
 
-  let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+	let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
 
-  if (courseId && state.courses.length > 0) {
-    course = getCourseById(state.courses, courseId);
-  }
+	if (courseId && state.courses.length > 0) {
+		course = getCourseById(state.courses, courseId);
+	}
 
 	const authorsFormattedForDropdown = state.authors.map((author) => {
 		return {
