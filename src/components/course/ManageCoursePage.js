a8d19e50eb56b49bd0278e5f05fe60bbaced1f18
anorman68 <anorman68@gmail.com>
2018-08-16 16:06:25 -0400

    after module 14: Testing Redux

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import { authorsFormattedForDropdown } from '../../selectors/selectors';
 import toastr from 'toastr';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
 	constructor(props, context) {
 		super(props, context);
 
@@ -31,10 +32,29 @@ class ManageCoursePage extends React.Component {
 		let course = Object.assign({}, this.state.course);
 		course[field] = event.target.value;
 		return this.setState({ course: course });
-	}
+  }
+
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
 
 	saveCourse(event) {
-		event.preventDefault();
+    event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
 		this.setState({ saving: true });
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
@@ -75,6 +95,7 @@ ManageCoursePage.contextTypes = {
 	router: PropTypes.object
 };
 
+
 function getCourseById(courses, id) {
 	const course = courses.filter((course) => course.id == id);
 	if (course.length) return course[0]; //since filter returns an array, have to grab the first.
@@ -90,16 +111,9 @@ function mapStateToProps(state, ownProps) {
 		course = getCourseById(state.courses, courseId);
 	}
 
-	const authorsFormattedForDropdown = state.authors.map((author) => {
-		return {
-			value: author.id,
-			text: author.firstName + ' ' + author.lastName
-		};
-	});
-
 	return {
 		course: course,
-		authors: authorsFormattedForDropdown
+		authors: authorsFormattedForDropdown(state.authors)
 	};
 }
 
